32a562fe5500224f46100b302e4689ea62e86ba2
Christian Bermejo Guerrero <cbermejo@gamma14.aulas.gsyc.urjc.es>
2017-02-07 10:30:02 +0100

    Calculadora

--- calculadora.py
+++ calculadora.py
@@ -4,27 +4,31 @@
 # Christian Bermejo Guerrero
 import sys
 
+if len(sys.argv) != 4:
+    sys.exit("Usage: calculadora.py operacion operando1 operando2")
+
 def suma(operando1, operando2):
-    return int(operando1) + int(operando2)
+    return float(operando1) + float(operando2)
 
 def resta(operando1, operando2):
-    return int(operando1) - int(operando2)
+    return float(operando1) - float(operando2)
 
 def multiplicar(operando1, operando2):
-    return int(operando1) * int(operando2)
+    return float(operando1) * float(operando2)
 
 def dividir(operando1, operando2):
-    return int(operando1) / int(operando2)
-try:
-    if (sys.argv[1] == 'suma'):
-        print (suma(sys.argv[2],sys.argv[3]))
-    elif (sys.argv[1] == 'resta'):
-        print (resta(sys.argv[2],sys.argv[3]))
-    elif (sys.argv[1] == 'multiplicar'):
-        print (multiplicar(sys.argv[2],sys.argv[3]))
-    elif (sys.argv[1] == 'dividir'):
-        print (dividir(sys.argv[2],sys.argv[3]))
-    else:
-        print ('Operacion no valida')
-except ZeroDivisionError:
-    print('No debes dividir entre 0')
+    return float(operando1) / float(operando2)
+
+if (sys.argv[1] == 'suma'):
+    print (suma(sys.argv[2],sys.argv[3]))
+elif (sys.argv[1] == 'resta'):
+    print (resta(sys.argv[2],sys.argv[3]))
+elif (sys.argv[1] == 'multiplicar'):
+    print (multiplicar(sys.argv[2],sys.argv[3]))
+elif (sys.argv[1] == 'dividir'):
+    try:
+        print(dividir(sys.argv[2],sys.argv[3]))
+    except ZeroDivisionError:
+        print ("No debes dividir entre 0")
+else:
+    print ('Operacion no valida')
